e0a4c333bce831414f6b7720fd62d96978ce9302
Yusuf Ozdemir <numberone.fatalrhymer@gmail.com>
2011-08-02 08:52:08 +0300

    updated charset selection modifications from last push to auto detect charset instead

--- Datatables.php
+++ Datatables.php
@@ -4,7 +4,7 @@
   *
   * This is a wrapper class/library for Datatables (table plug-in for Jquery)
   *
-  * @version    0.5.4 ( Standalone Php Library )
+  * @version    0.6 ( Standalone Php Library )
   * @author     Vincent Bambico <metal.conspiracy@gmail.com>
   *             Yusuf Ozdemir <yusuf@ozdemir.be>
   *
@@ -186,15 +186,14 @@
     /**
     * Builds all the necessary query segments and performs the main query based on results set from chained statements
     *
-    * @param string charset
     * @return string
     */
-    public function generate($charset = 'UTF-8')
+    public function generate()
     {
       $this->get_paging();
       $this->get_ordering();
       $this->get_filtering();
-      return $this->produce_output($charset);
+      return $this->produce_output();
     }
 
     /**
@@ -282,10 +281,9 @@
     /**
     * Builds a JSON encoded string data
     *
-    * @param string charset
     * @return string
     */
-    protected function produce_output($charset)
+    protected function produce_output()
     {
       $aaData = array();
       $rResult = $this->get_display_result();
@@ -324,10 +322,7 @@
         'sColumns'             => implode(',', $sColumns)
       );
 
-      if(strtolower($charset) == 'utf-8')
-        return json_encode($sOutput);
-      else
-        return $this->jsonify($sOutput);
+      return $this->jsonify($sOutput);
     }
 
     /**
@@ -511,12 +506,14 @@
 
       if($isList)
       {
-        foreach($result as $value) $json[] = $this->jsonify($value);
+        foreach($result as $value)
+          $json[] = $this->jsonify($value);
         return '[' . join(',', $json) . ']';
       }
       else
       {
-        foreach($result as $key => $value) $json[] = $this->jsonify($key) . ':' . $this->jsonify($value);
+        foreach($result as $key => $value)
+          $json[] = $this->jsonify($key) . ':' . $this->jsonify($value);
         return '{' . join(',', $json) . '}';
       }
     }
